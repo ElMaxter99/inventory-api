16d81c3bec4cbf1dd70b3bfa6dc7648f4a34b785
Alvaro Amorós <39102625+ElMaxter99@users.noreply.github.com>
2026-01-12 22:46:15 +0100

    Fix logger dependency for pino-pretty

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,9 +1,12 @@
 const pino = require("pino");
 
-const logger = pino({
-  transport: {
-    target: "pino-pretty",
-  },
-});
+const logger =
+  process.env.NODE_ENV === "production"
+    ? pino()
+    : pino({
+        transport: {
+          target: "pino-pretty",
+        },
+      });
 
 module.exports = logger;

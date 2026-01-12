3e59b984a1261de2c53e5b148178a0209c4d04f1
Alvaro Amorós <39102625+ElMaxter99@users.noreply.github.com>
2026-01-12 23:21:22 +0100

    Handle unhandled rejections

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const app = require("./app");
 const config = require("./config");
 const { connectMongoose } = require("./db/mongoose");
+const logger = require("./utils/logger");
+
+process.on("unhandledRejection", (reason) => {
+  logger.error({ err: reason }, "Unhandled promise rejection");
+});
+
+process.on("uncaughtException", (err) => {
+  logger.error({ err }, "Uncaught exception");
+  process.exit(1);
+});
 
 async function main() {
   await connectMongoose();
